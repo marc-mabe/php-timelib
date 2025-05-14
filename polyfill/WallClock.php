f6fd35be997699c06576a1d9fd61e37cfff6cd43
Marc Bennewitz <m.bennewitz@dcmn.com>
2025-05-14 08:10:33 +0200

    Optional zone, calendar and week info for Clock->takeZonedDateTime()

--- polyfill/WallClock.php
+++ polyfill/WallClock.php
@@ -52,9 +52,12 @@ final class WallClock implements Clock
         return Instant::fromUnixTimestampTuple(($this->timer)());
     }
 
-    public function takeZonedDateTime(Zone $zone): ZonedDateTime
-    {
-        return $this->takeInstant()->toZonedDateTime($zone);
+    public function takeZonedDateTime(
+        ?Zone $zone = null,
+        ?Calendar $calendar = null,
+        ?WeekInfo $weekInfo = null
+    ): ZonedDateTime {
+        return $this->takeInstant()->toZonedDateTime($zone, $calendar, $weekInfo);
     }
 
     public function takeUnixTimestamp(TimeUnit $unit = TimeUnit::Second, bool $fractions = false): int|float
